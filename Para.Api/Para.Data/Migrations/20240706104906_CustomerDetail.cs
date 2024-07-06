310d68c61f76f8b6ccecfad58c7f40c8c008a82a
recep <recepysl@outlook.com>
2024-07-06 13:52:46 +0300

    migration

---
+++ Para.Api/Para.Data/Migrations/20240706104906_CustomerDetail.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Para.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class CustomerDetail : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CustomerDetail",
+                schema: "dbo",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CustomerId = table.Column<long>(type: "bigint", nullable: false),
+                    FatherName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    MotherName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    EducationStatus = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    MontlyIncome = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Occupation = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    InsertUser = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    InsertDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CustomerDetail", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CustomerDetail_Customer_CustomerId",
+                        column: x => x.CustomerId,
+                        principalSchema: "dbo",
+                        principalTable: "Customer",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CustomerDetail_CustomerId",
+                schema: "dbo",
+                table: "CustomerDetail",
+                column: "CustomerId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CustomerDetail",
+                schema: "dbo");
+        }
+    }
+}
